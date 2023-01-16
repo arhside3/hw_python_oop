8c33d565f45534e17c37cc01756101854a40aba4
arhside3 <122724220+arhside3@users.noreply.github.com>
2023-01-17 00:52:25 +0300

    Update homework.py

--- homework.py
+++ homework.py
@@ -94,8 +94,8 @@ class SportsWalking(Training):
 
     def get_spent_calories(self) -> float:
         spent_calories = ((self.CF_WALK_1 * self.weight
-                          + (self.get_mean_speed() * self.KMH_IN_MSEC
-                          ** self.CF_WALK_2 / self.height)
+                          + (pow((self.get_mean_speed() * self.KMH_IN_MSEC), 2)
+                          / (self.height / self.CM_IN_M))
                           * self.CF_WALK_3 * self.weight)
                           * self.duration * self.M_IN_H)
         return spent_calories
