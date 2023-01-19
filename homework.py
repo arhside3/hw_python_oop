29e905bf36ed09590627f534e8bed19290e5dc86
arhside3 <122724220+arhside3@users.noreply.github.com>
2023-01-20 00:39:20 +0300

    Update homework.py

--- homework.py
+++ homework.py
@@ -67,8 +67,9 @@ class Running(Training):
 
         return ((self.CALORIES_MEAN_SPEED_MULTIPLIER
                  * self.get_mean_speed()
-                 + self.CALORIES_MEAN_SPEED_SHIFT) * self.weight
-                  / self.M_IN_KM * self.duration * self.M_IN_H)
+                 + self.CALORIES_MEAN_SPEED_SHIFT) 
+                 * self.weight / self.M_IN_KM 
+                 * self.duration * self.M_IN_H)
 
 
 @dataclass
